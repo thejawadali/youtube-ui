aad3ef62b93ae4a611489a78d5a88d97351c4b4b
Jawad Ali <alijamil734@gmail.com>
2021-08-08 17:29:09 +0500

    initialized project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        youtube: '#FF2500'
+      },
+      fontSize: {
+        xxs: ".5rem"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
